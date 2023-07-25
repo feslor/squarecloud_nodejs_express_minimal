a8e30277416b14cf7db09548277afed3800dd994
feslor <116938817+feslor@users.noreply.github.com>
2023-07-25 03:35:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 // Web applications need at least 512Mb of RAM.
 // If you see the error "BLOCKED Website..." wait 5 minutes
 // and try again otherwise open a support ticket.
-// Change the "DOMAIN" name in the squarecloud.app file to
+// Change the "SUBDOMAIN" name in the squarecloud.app file to
 // anything else before uploading the zip file.
 app.listen(80, () => {
   console.log('Server is running go to "Network" tab to see your domain.');
